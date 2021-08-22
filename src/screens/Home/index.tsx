ec9519648459d99e77e018af762176dcddbf99ba
Minh Le <quangminh0001@gmail.com>
2021-08-23 00:41:07 +0800

    Add refresh and setting actions

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,9 @@ import HomeScreenContainer from "../../containers/HomeScreenContainer";
 const HomeScreen = (): React.ReactElement => (
   <>
     <Appbar.Header>
+      <Appbar.Action icon="refresh" onPress={() => {}} />
       <Appbar.Content title="ETH Test" />
+      <Appbar.Action icon="cog" onPress={() => {}} />
     </Appbar.Header>
     <HomeScreenContainer />
   </>
